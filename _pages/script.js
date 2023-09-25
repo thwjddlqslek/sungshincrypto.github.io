954818caea90719843a764fc4b31afdcd115d765
sungshincrypto <sungshincrypto@gmail.com>
2023-09-26 05:18:53 +0900

    js

---
+++ _pages/script.js
@@ -0,0 +1,12 @@
+let curPos = 0;
+let positionValue = 0;
+const IMAGE_WIDH = 500;
+
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
+const images = document.querySelector(".images");
+
+function init(){
+    leftBtn.setAttribute('disabled', 'true');
+}
+intit();
